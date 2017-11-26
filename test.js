8e69b970a9961a56f16cd24d03e374bece3b7e45
MIke Alexeenko <mike.alexeenko@gmail.com>
2017-11-26 14:52:41 +0200

    Doc html start

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-console.log(document.querySelector("div"));
+/*
+ var int_start = +prompt('enter start count', 'aa');
+ var int_end = +prompt('enter start count', 'bb');
+ var i;
+ for (i = int_start; i <= int_end; i++) {
+ if (i % 2 == 0) {
+ document.writeln(i);
+ console.log(i);
+ }
+ }
+ */
+
+var int_start1 = +prompt('enter start count', 'aa');
+var int_end1 = +prompt('enter start count', 'bb');
+var i;
+for (i = int_start1; i <= int_end1; i++) {
+    i += i;
+    console.log(i);
+}

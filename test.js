d5550436c4683da2b2740e693e77695b85109326
MIke Alexeenko <mike.alexeenko@gmail.com>
2017-12-05 08:23:39 +0200

    fibonacci & isPrime

--- test.js
+++ test.js
@@ -10,10 +10,19 @@
  }
  */
 
-var int_start1 = +prompt('enter start count', 'aa');
-var int_end1 = +prompt('enter start count', 'bb');
-var i;
-for (i = int_start1; i <= int_end1; i++) {
-    i += i;
-    console.log(i);
+"use strict";
+var test_obj = {
+    aa: 'Aa',
+    bb: 'bb',
+    dd: 'Ddd',
+    cc: 'cc'
 }
+var new_arr = [];
+var new_arr1 = [];
+for (var key in test_obj) {
+    new_arr.push(test_obj[key]);
+    new_arr1.push(key);
+}
+
+console.log(new_arr1);
+console.log(new_arr.sort());

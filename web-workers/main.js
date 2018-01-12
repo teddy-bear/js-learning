05523a3f9f9e22638045bec5a31fb146cb3c4ecc
M Aleksieienko <m.aleksieienko@mobilunity.com>
2018-01-12 15:15:39 +0200

    edit worker

--- web-workers/main.js
+++ web-workers/main.js
@@ -2,26 +2,35 @@ var first = document.querySelector('#number1');
 var second = document.querySelector('#number2');
 
 var result = document.querySelector('.result');
+var controls = document.querySelector('.controls');
 
-if (window.Worker) { // Check if Browser supports the Worker api.
-	// Requires script name as input
-	var myWorker = new Worker("worker.js");
+// Check if Browser supports the Worker api.
+if (window.Worker) {
+    // Requires script name as input
+    var myWorker = new Worker("worker.js");
 
-// onkeyup could be used instead of onchange if you wanted to update the answer every time
-// an entered value is changed, and you don't want to have to unfocus the field to update its .value
+    var i = 0;
 
-	first.onchange = function() {
-	  myWorker.postMessage([first.value,second.value]); // Sending message as an array to the worker
-	  console.log('Message posted to worker');
-	};
+    // onkeyup could be used instead of onchange if you wanted to update the answer every time
+    // an entered value is changed, and you don't want to have to unfocus the field to update its .value
+    first.onkeyup = function () {
+        myWorker.postMessage([first.value, second.value, controls.i]); // Sending message as an array to the worker
+        console.log('First input sent to worker');
+    };
 
-	second.onchange = function() {
-	  myWorker.postMessage([first.value,second.value]);
-	  console.log('Message posted to worker');
-	};
+    second.onchange = function () {
+        myWorker.postMessage([first.value, second.value, controls.i]);
+        console.log('Second input posted to worker');
+    };
 
-	myWorker.onmessage = function(e) {
-		result.textContent = e.data;
-		console.log('Message received from worker');
-	};
+    controls.onclick = function () {
+        i++;
+        myWorker.postMessage([first.value, second.value, i]);
+        console.log("Following " + i + " has been sent");
+    };
+
+    myWorker.onmessage = function (e) {
+        result.textContent = e.data;
+        console.log('Message received from worker');
+    };
 }
--- web-workers/worker.js
+++ web-workers/worker.js
@@ -1,6 +1,7 @@
-onmessage = function(e) {
-  console.log('Message received from main script');
-  var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
-  console.log('Posting message back to main script');
-  postMessage(workerResult);
+onmessage = function (e) {
+    console.log('Message received from main script');
+    var temRes = e.data[2] + ' times clicked';
+    var workerResult = 'Result: ' + (e.data[0] * e.data[1]) + " " + temRes;
+    console.log('Posting message back to main script');
+    postMessage(workerResult);
 }

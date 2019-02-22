1974cb8c11c41240e16542c747e57c41d3cc8803
Mike Alexeenko <myal@ciklum.com>
2019-02-22 10:48:59 +0200

    edits for the objects

--- test.js
+++ test.js
@@ -1,18 +1,64 @@
-/*
- var int_start = +prompt('enter start count', 'aa');
- var int_end = +prompt('enter start count', 'bb');
- var i;
- for (i = int_start; i <= int_end; i++) {
- if (i % 2 == 0) {
- document.writeln(i);
- console.log(i);
- }
- }
- */
-
 "use strict";
 
-var foo1 = ["one", "two", "three"];
+var salaries = {
+    "aa": 'dfdfdf',
+    "bb": '300',
+    "cc": 550,
+    "d": 250
+};
 
-// с деструктуризацией
-var [aa, bb, cc] = foo1;
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+}
+
+function doubleIntValues(obj) {
+    var key;
+
+    for (key in obj) {
+        if (!isNumeric(obj[key])) continue;
+        obj[key] = obj[key] * 2;
+    }
+
+    console.log(obj);
+}
+
+function getMax(obj) {
+    var key,
+        current_int = 0,
+        current_key,
+        max_key,
+        max_int = 0;
+
+    for (key in obj) {
+        current_int = obj[key];
+        current_key = key;
+        if (current_int > max_int) {
+            max_int = current_int;
+            max_key = current_key;
+        }
+    }
+
+    console.log(max_key);
+}
+
+
+function getSumm(obj) {
+    var key,
+        key_int,
+        summ = 0;
+
+    for (key in obj) {
+        key_int = Number(obj[key]);
+        if (isNaN(key_int)) continue;
+        summ += key_int;
+    }
+    console.log(summ);
+}
+
+var call_max = doubleIntValues;
+
+(function () {
+    call_max(salaries);
+}());
+

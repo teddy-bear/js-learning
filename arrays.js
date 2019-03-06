522e4d0b43b9567215ac19df0291042606c5ea49
Mike Alexeenko <myal@ciklum.com>
2019-03-06 15:11:36 +0200

    Leave uniques values in array

--- arrays.js
+++ arrays.js
@@ -1,3 +1,31 @@
+/**
+ * Documentation
+ */
+var arr = [1, 2, 3, {}, 'cc'];
+
+//многомерный массив
+var matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+var newArr = arr.concat(matrix); //складывает 2 массива и возвращает новый
+newArr.pop(); // remove last
+newArr.push('the very last'); // add last
+newArr.shift(); // remove first;
+newArr.unshift('the very first'); // add first
+
+var arr_splice = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+arr_splice.splice(-1, 1, 'replaced 1st', 'replaced 2nd'); // начиня с позиции -2 удалить 2 эл-та и встаивить на их место новые эл-ты
+
+// splice - изменяет текущий массив
+// slice - получает вырезку в новый массив(задать переменную) без именения исходного массива
+
+var sliced = newArr.slice(1, 3); // с какого по какой эл-т массива взять, исходный массив при этом не меняется
+var newArr2 = newArr.join('/');
+
 var my_obj = {
 
         options: {
@@ -30,63 +58,49 @@ var my_obj = {
 
             /*var arr_sorted = unique(obj.received);
             return arr_sorted;*/
+        },
+
+        /**
+         * Get unique values from array
+         * @param arr
+         * @returns {string[]}
+         */
+        uniqueSlow: function (arr) {
+            var result_arr = [];
+            nextInput:
+                for (var i = 0; i < arr.length; i++) {
+                    var current_val = arr[i];
+                    for (var j = 0; j < result_arr.length; j++) {
+                        if (result_arr[j] === current_val) continue nextInput;
+                    }
+                    result_arr.push(current_val);
+                }
+            return result_arr;
+        },
+
+        /**
+         * Get unique values from array
+         * @param arr
+         * @returns {string[]}
+         */
+        uniqueFast: function (arr) {
+            var obj = {},
+                str = '',
+                i = 0;
+
+            for (i; i < arr.length; i++) {
+                str = arr[i];
+                obj[str] = i; // запомнить строку в виде свойства объекта
+            }
+
+            return Object.keys(obj); // или собрать ключи перебором для IE8-
         }
+
     }
 ;
-
-var arr = [1, 2, 3, {}, 'cc'];
-
-//многомерный массив
-var matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-];
-//console.log(matrix[2][0]);
-
-var newArr = arr.concat(matrix); //складывает 2 массива и возвращает новый
-newArr.pop(); // remove last
-newArr.push('the very last'); // add last
-newArr.shift(); // remove first;
-newArr.unshift('the very first'); // add first
-
-var arr_splice = ['a', 'b', 'c', 'd', 'e', 'f'];
-
-arr_splice.splice(-1, 1, 'replaced 1st', 'replaced 2nd'); // начиня с позиции -2 удалить 2 эл-та и встаивить на их место новые эл-ты
-
-// splice - изменяет текущий массив
-// slice - получает вырезку в новый массив(задать переменную) без именения исходного массива
-
-var sliced = newArr.slice(1, 3); // с какого по какой эл-т массива взять, исходный массив при этом не меняется
-
-console.log(arr_splice);
-var newArr2 = newArr.join('/');
-
-function unique(arr) {
-    var result_arr = [];
-    nextInput:
-        for (var i = 0; i < arr.length; i++) {
-            var current_val = arr[i];
-            for (var j = 0; j < result_arr.length; j++) {
-                if (result_arr[j] === current_val) continue nextInput;
-            }
-            result_arr.push(current_val);
-        }
-    return result_arr;
-}
-
-function unique2(arr) {
-    var obj = {};
-
-    for (var i = 0; i < arr.length; i++) {
-        var str = arr[i];
-        obj[str] = true; // запомнить строку в виде свойства объекта
-    }
-
-    return Object.keys(obj); // или собрать ключи перебором для IE8-
-}
 
 
 (function (_) {
     _.arrClean(_.options.sample_array);
+    _.uniqueFast(_.options.sample_array);
 }(my_obj));
